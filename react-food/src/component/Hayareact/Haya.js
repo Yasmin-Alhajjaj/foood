0bac27d7e5cfad4fbc9f627fd8b74a7cf52dd552
Owner <you@example.com>
2019-09-09 16:15:38 +0300

    run

---
+++ react-food/src/component/Hayareact/Haya.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from "react";
+import axios from "axios";
+import UploadImg from './Components/UploadImg'
+export default class App extends Component {
+    state = {};
+  
+    render() {
+      return(
+     <>
+      <UploadImg />
+      </>
+      ); 
+    }
+  }
+  
